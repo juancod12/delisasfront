3217c726acb5eb209673aa9deee4efa63d1f6695
juanco12 <j12drh@gmail.com>
2026-05-12 23:05:09 -0500

    url

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -11,7 +11,7 @@ const handleSubmit = async (e) => {
   e.preventDefault();
 
   try {
-    const res = await fetch("http://localhost:8080/auth/login", {
+     const res = await fetch(`${import.meta.env.VITE_API_URL}/auth/login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
